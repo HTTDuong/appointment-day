63398bdda0f625d33115a95d217d439cd78bee64
Ha Thi Thuy Duong <52100542@student.tdtu.edu.vn>
2023-08-25 15:53:32 +0700

    undate detail infor doctor

--- Nodejs/src/route/web.js
+++ Nodejs/src/route/web.js
@@ -2,6 +2,7 @@
 import express from "express";
 import homeController from "../controllers/homeController";
 import userController from "../controllers/userController";
+import doctorController from "../controllers/doctorController";
 
 let router = express.Router();
 
@@ -20,6 +21,10 @@ let initWebRoutes = (app) => {
     router.put('/api/edit-user', userController.handleEditUser);
     router.delete('/api/delete-user', userController.handleDeleteUser);
     router.get('/api/allcode', userController.getAllCode);
+
+    router.get('/api/top-doctor-home',doctorController.getTopDoctorHome);
+    router.get('/api/get-all-doctors',doctorController.getAllDoctors);
+    router.post('/api/save-infor-doctors',doctorController.postInforDoctor);
 
     //rest api (get post delete put)
 
