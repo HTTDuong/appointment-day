669717b2339020fa5b79442d8b5c567c7f4d791b
Ha Thi Thuy Duong <52100542@student.tdtu.edu.vn>
2023-08-30 23:03:18 +0700

    layout detail doctor

--- Nodejs/src/services/doctorService.js
+++ Nodejs/src/services/doctorService.js
@@ -92,7 +92,7 @@ let getDetailDoctorById = (inputId) => {
                         id: inputId
                     }, 
                     attributes: {
-                        exclude: ['password', 'image']
+                        exclude: ['password']
                     }, 
                     include: [
                         { 
@@ -105,9 +105,15 @@ let getDetailDoctorById = (inputId) => {
                             attributes: ['valueEn', 'valueVi']
                         },
                     ],
-                    raw: true,
+                    raw: false,
                     nest: true
                 })
+
+                if(data && data.image){
+                    data.image = new Buffer(data.image, 'base64').toString('binary');
+                }
+
+                if(!data) data = {};
 
                 resolve({
                     errCode: 0,
