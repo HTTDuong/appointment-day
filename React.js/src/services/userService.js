e97f3ebfa892db141e4bc7260845eec903f695a5
Ha Thi Thuy Duong <52100542@student.tdtu.edu.vn>
2023-09-09 17:10:22 +0700

    save infor client booking

--- React.js/src/services/userService.js
+++ React.js/src/services/userService.js
@@ -58,6 +58,13 @@ const getExtraInforDoctorById = (doctorId) => {
     return axios.get(`/api/get-extra-infor-doctor-by-id?doctorId=${doctorId}`)
 }
 
+const getProfileDoctorById = (doctorId) => {
+    return axios.get(`/api/get-profile-doctor-by-id?doctorId=${doctorId}`)
+}
+
+const postPatientBookingAppointment = (data) => {
+    return axios.post('/api/patient-book-appointment', data)
+}
 
 export {
     handleLoginApi, getAllUsers,
@@ -66,5 +73,6 @@ export {
     getTopDoctorHomeService, getAllDoctors,
     saveDetailDoctorService, getDetailInforDoctor,
     saveBulkScheduleDoctor, getScheduleDoctorByDate,
-    getExtraInforDoctorById
+    getExtraInforDoctorById, getProfileDoctorById,
+    postPatientBookingAppointment
 }
